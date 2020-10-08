0c5497df3148fffcd5962d0a0e65da0ddb1ba0c3
andfxx27 <idea.andersen@gmail.com>
2020-10-08 11:46:58 +0700

    Add section

--- index.php
+++ index.php
@@ -10,8 +10,12 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
+    <!-- Google's Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <!-- Author's own CSS -->
     <link rel="stylesheet" href="css/helper.css">
+    <link rel="stylesheet" href="css/reset.css">
 </head>
 
 <body>
@@ -25,7 +29,7 @@
 
             <ul class="right hide-on-med-and-down">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Popular posts</a></li>
+                <li><a href="#">Create post</a></li>
                 <li><a href="#">Register</a></li>
             </ul>
         </div>
@@ -34,21 +38,52 @@
     <!-- Below list will be shown instead on medium screen (tablet) and down -->
     <ul class="sidenav" id="mobile-demo">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Popular posts</a></li>
+        <li><a href="#">Create post</a></li>
         <li><a href="#">Register</a></li>
     </ul>
 
     <!-- Bunch of past forum post -->
-    <main>
+    <main class="row">
         <!-- List represent several number of post -->
-        <ul class="collection">
-            <li class="collection-item avatar">
-                <i class="material-icons circle">folder</i>
-                <span class="title">Title</span>
-                <p>Forum's headline</p>
-                <a href="#" class="secondary-content"><i class="material-icons">grade</i></a>
-            </li>
-        </ul>
+        <div class="col s9 left">
+            <h1>Posts</h1>
+            <ul class="collection">
+                <li class="collection-item avatar">
+                    <i class="material-icons circle">folder</i>
+                    <span class="title"><a href="#">Title 1</a></span>
+                    <p>Forum's headline 1</p>
+                    <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum labore repudiandae beatae odio culpa voluptatibus obcaecati nulla nesciunt quam in similique animi commodi soluta illo, iusto aperiam sed aut nemo deleniti quae sapiente cumque nobis. Illum distinctio veritatis consequatur fugit consectetur? Odio unde dolorum voluptate adipisci libero dolor aut at.</p>
+                    <a href="#" class="secondary-content"><i class="material-icons">keyboard_arrow_right</i></a>
+                </li>
+                <li class="collection-item avatar">
+                    <i class="material-icons circle">folder</i>
+                    <span class="title"><a href="#">Title 2</a></span>
+                    <p>Forum's headline 2</p>
+                    <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nam delectus, harum alias quod quidem reprehenderit vitae placeat maxime laboriosam. Repudiandae doloremque debitis sunt officiis ratione veniam, iusto possimus nulla tempore aperiam ipsum molestias iure exercitationem, fugiat rerum. Enim alias ab corporis, dolore amet exercitationem tenetur sed iste illo est.</p>
+                    <a href="#" class="secondary-content"><i class="material-icons">keyboard_arrow_right</i></a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- List forum post from most likes to least likes count -->
+        <div class="col s3 right hide-on-med-and-down">
+            <h2>Popular</h2>
+            <div class="divider"></div>
+            <div class="section">
+                <h5>Section 1</h5>
+                <p>Stuff</p>
+            </div>
+            <div class="divider"></div>
+            <div class="section">
+                <h5>Section 2</h5>
+                <p>Stuff</p>
+            </div>
+            <div class="divider"></div>
+            <div class="section">
+                <h5>Section 3</h5>
+                <p>Stuff</p>
+            </div>
+        </div>
     </main>
 
     <!-- Compiled and minified JavaScript -->
